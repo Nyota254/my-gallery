b3ac80e1ca3e1224cff49e3833640ace31805925
Nyota254 <knyota66@gmail.com>
2019-10-04 09:04:06 +0300

    register the images app and routing to the urls.py files

--- images/views.py
+++ images/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    '''
+    This method will display the home page of the application
+    '''
+    title = "Home"
+    context = {
+        "title":title
+    }
+
+    return render(request,'index.html',context)
