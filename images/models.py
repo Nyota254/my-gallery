dd2090dfbf7be8cc0c4fdbbdf2fc28866657cd5d
Nyota254 <knyota66@gmail.com>
2019-10-04 14:48:08 +0300

    add tests for behaviours and sceleton to models

--- images/models.py
+++ images/models.py
@@ -1,3 +1,91 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    '''
+    This class will contain the Images model
+    '''
+    pass
+    def save_image(self):
+        '''
+        Saves data to database from the image fields
+        '''
+        pass
+
+    def delete_image(self):
+        '''
+        Deletes image data from database
+        '''
+        pass
+
+    def update_image(self):
+        '''
+        Updates data of the image in the database
+        '''
+        pass
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        '''
+        Querys the database to get the images by id
+        '''
+        pass
+
+    @classmethod
+    def search_image(cls,category):
+        '''
+        Querys the database to get the images by category
+        '''
+        pass
+    @classmethod
+    def filter_by_location(cls,location):
+        '''
+        Querys the database to search for images by their location
+        '''
+        pass
+
+class Location(models.Model):
+    '''
+    This class will contain the location model
+    '''
+    pass
+
+    def save_location(self):
+        '''
+        Saves the location data to the database
+        '''
+        pass
+    
+    def delete_location(self):
+        '''
+        Deletes the location from the database
+        '''
+        pass
+    def update_location(self):
+        '''
+        Updates the location from the database
+        '''
+        pass
+
+class Category(models.Model):
+    '''
+    This class will contain the Category model
+    '''
+    pass
+
+    def save_category(self):
+        '''
+        Saves the category data to the database
+        '''
+        pass
+    
+    def delete_category(self):
+        '''
+        Deletes the category from the database
+        '''
+        pass
+    def update_category(self):
+        '''
+        Updates the category from the database
+        '''
+        pass
+
--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,88 @@
 from django.test import TestCase
+from .models import Image,Location,Category
 
-# Create your tests here.
+class ImageTest(TestCase):
+    '''
+    Test class for the Image model
+    '''
+    def setUp(self):
+        '''
+        Sets up the tests
+        '''
+        # self.new_image = Image(image='media/images.png',image_name='moringa_day1',image_description='this is my first pic of my first day in moringa')
+        self.new_location = Location(location='nairobi')
+        self.new_location.save()
+        self.new_category = Category(category='studies')
+        self.new_category.save()
+        self.new_image = Image(image='media/images.png',image_name='moringa_day1',image_description='this is my first pic of my first day in moringa',image_location=self.new_location,image_category=self.new_category)
+        self.new_image.save()
+
+    def tearDown(self):
+        '''
+        clears the database after every test
+        '''
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+        
+    def test_instance(self):
+        '''
+        Test Whether the image object is an instance of the model class
+        '''
+        self.assertTrue(isinstance(self.new_image,Image))
+
+    def test_save_method(self):
+        '''
+        Test whether the class method save_image works 
+        '''
+        self.new_image.save_image()
+        saved_images = Image.objects.all()
+        self.assertTrue(len(saved_images)>1)
+
+    def test_delete_method(self):
+        '''
+        Test whether the class method delete_image works
+        '''
+        self.new_image.delete_image()
+        self.assertTrue(len(saved_images)==0)
+
+    def test_update_method(self):
+        '''
+        Test whether the class method update_image works
+        '''
+        self.new_image.update(image_name ='moringa-firstday')
+        searched_image = Image.objects.filter(id=1)
+        self.assertEqual(searched_image.image_name,'moringa-firstday')
+
+    def test_get_image_by_id(self):
+        '''
+        Test whether this class method can search for an image using its id
+        '''
+        self.another_image = Image(image='media/another_image.png',image_name='moringa_day2',image_description='this is my first pic of my second day in moringa',image_location=self.new_location,image_category=self.new_category)
+        self.another_image.save()
+        searched_image = Image.objects.filter(id=2)
+        self.assertEqual(searched_image.image,'media/another_image.png')
+
+    def test_get_image_by_location(self):
+        '''
+        Test whether this class method can search for an image using its id
+        '''
+        self.another_location = Location(location='mombasa')
+        self.another_location.save()
+        self.another_image = Image(image='media/another_image.png',image_name='moringa_day2',image_description='this is my first pic of my second day in moringa',image_location=self.another_location,image_category=self.new_category)
+        self.another_image.save()
+        searched_image = Image.objects.filter(location='mombasa')
+        self.assertEqual(searched_image.image,'media/another_image.png')
+
+    def test_get_image_by_category(self):
+        '''
+        Test whether this class method can search for an image using its id
+        '''
+        self.another_category = Category(category='fun')
+        self.another_category.save()
+        self.another_image = Image(image='media/another_image.png',image_name='moringa_day2',image_description='this is my first pic of my second day in moringa',image_location=self.new_location,image_category=self.another_category)
+        self.another_image.save()
+        searched_image = Image.objects.filter(category=fun)
+        self.assertEqual(searched_image.image,'media/another_image.png')
+
+    
