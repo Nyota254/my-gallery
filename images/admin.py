c3972303c3e89959ccacfe0ab3e91f1871d17bbc
Nyota254 <knyota66@gmail.com>
2019-10-05 09:49:45 +0300

    add the models with the admin page

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Tag,Image,Category,Location
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags',)
+
+admin.site.register(Tag)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image,ImageAdmin)
 
 # Register your models here.
